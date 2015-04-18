797175b7e8881f0b1433ef34695cf2037b3ada60
islomar <islomar@gmail.com>
2015-04-18 21:44:37 +0200

    Parsing high score list to CSV works

--- src/main/java/com/king/minigame/controller/ListController.java
+++ src/main/java/com/king/minigame/controller/ListController.java
@@ -5,7 +5,10 @@ import com.king.minigame.score.ScoreService;
 import com.king.minigame.session.SessionService;
 
 import java.time.Clock;
+import java.time.Instant;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -22,12 +25,23 @@ public class ListController {
   public String getHighScoreListForLevel(Integer levelId) {
 
     Map<Integer, Score> highScoreList = scoreService.getHighScoreListForLevel(levelId);
+    //highScoreList = hardcode();
+
     String highScoreListInCsvFormat = parseToCsv(highScoreList);
     return highScoreListInCsvFormat;
   }
 
-  private String parseToCsv(Map<Integer, Score> highScoreList) {
+  private Map<Integer, Score> hardcode() {
 
-    return null;
+    Map<Integer, Score> response = new HashMap<Integer, Score>();
+    response.put(111, new Score(100, Instant.now()));
+    response.put(111, new Score(300, Instant.now()));
+    response.put(222, new Score(200, Instant.now()));
+
+    return response;
+  }
+
+  private String parseToCsv(Map<Integer, Score> highScoreList) {
+    return highScoreList.entrySet().stream().map(s -> String.format("%d=%d", s.getKey(), s.getValue().getScoreValue())).collect(Collectors.joining(","));
   }
 }
